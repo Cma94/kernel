bf9d740c792a1410b4f1ef16920de4982244479a
Alberto <moracabanas@live.com>
2018-10-12 02:14:15 +0200

    - Updated $DIR obtaining to a non tar execution blocking expression (listed as DEPRECATED in a comment) - Added confirm function (NOT USED YET) - Added check_availability function - Modified download and uncompress callings to prevent execution if the requested file exist, passing them as parameters of check_availability function.

--- kernel.sh
+++ kernel.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 #Variables
-URL="https://git.kernel.org/torvalds/t/linux-4.19-rc6.tar.gz"
-TARGZ=$(basename $URL)
-DIR=`tar -tzf linux-4.19-rc6.tar.gz | head -1 | cut -f1 -d"/"`
+URL=https://git.kernel.org/torvalds/t/linux-4.19-rc6.tar.gz
+TARGZ=$(basename $URL) 
+#DEPRECATED DIR=`tar -tzf linux-4.19-rc6.tar.gz | head -1 | cut -f1 -d"/"`
+DIR=`echo "${TARGZ%.*.*}"`
 
 download(){
     wget $URL
@@ -13,8 +14,30 @@ uncompress(){
     tar -xvzf "$TARGZ"
 }
 
+confirm() {
+    # call with a promt
+    read -r -p "${1:-Are you sure? [y/N]}" response
+      case "$response" in
+         [yY][eE][sS]|[yY])
+            true
+            ;;
+         *)
+            false
+            ;;
+      esac
+}
+
+check_availability(){
+
+if [ -e $1 ]; then
+    echo "$1 already exist"
+else
+    $2
+fi
+}
+
 ###Program Begins###
 
-download
+check_availability $TARGZ download
 
-uncompress
+check_availability $DIR uncompress
